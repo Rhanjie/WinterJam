2312688dfacfcdb4635a414dbaf8d57e627a97b0
RhAnjiE <irhanjie@gmail.com>
2019-12-27 21:57:44 +0100

    Add initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 5.0f;
+
+    [HideInInspector] public Inventory inventory;
+
+    private void Start() {
+        inventory = GetComponent<Inventory>();
+    }
+
+    private void Update() {
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+        
+        transform.position += new Vector3(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime);
+
+        GetComponent<SpriteRenderer>().flipX = horizontal < 0;
+    }
+}
